21017bfa157fab1dfb2039804334701800db218e
Oscar Cuatrecasas <o.cuatrix@gmail.com>
2020-02-19 12:16:57 +0100

    Creating window

--- Smash-TV/Smash-TV/main.cpp
+++ Smash-TV/Smash-TV/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include "SDL/include/SDL.h"
+#pragma comment(lib, "SDL/lib/x86/SDL2.lib")
+#pragma comment(lib, "SDL/lib/x86/SDL2main.lib")
 
-using namespace std;
+#include <string>
 
-int main() {
-	cout << "Hello world!" << endl;
+int main(int argc, char* argv[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	std::string name = "window";
+	SDL_Window* window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0); //SDL_WINDOW_FULLSCREEN);
+
+	return 0;
 }
