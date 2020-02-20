5d4a365e71e80fabdd6cdcf54d13cbb7c5ea8e68
Oscar Cuatrecasas <o.cuatrix@gmail.com>
2020-02-20 22:37:01 +0100

    fixed

--- Smash-TV/Smash-TV/main.cpp
+++ Smash-TV/Smash-TV/main.cpp
@@ -7,8 +7,9 @@
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	std::string name = "window";
-	SDL_Window* window = SDL_CreateWindow(name.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, 0); //SDL_WINDOW_FULLSCREEN);
+	SDL_Window* window = SDL_CreateWindow("window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN); //SDL_WINDOW_FULLSCREEN);
+
+	SDL_Delay(5000);
 
 	return 0;
 }
